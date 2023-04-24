cae28fa0e684760e9e054d6296ed5eaaf2b95e0b
Maria Mills <maria@mariamills.org>
2023-04-24 13:59:20 -0400

    Add Abstract Enemy Class
    This commit includes the addition of a new abstract class called 'Enemy'. This class will serve as a template for creating different types of enemies in the game. By creating an abstract class, a common interface is defined with common attributes and methods that all enemies will have, while still allowing for customization and specialization of individual enemy types. This new class will help to improve the overall design and scalability of the game.

---
+++ Enemies/Enemy.cs
@@ -0,0 +1,11 @@
+﻿namespace StarterGame.Enemies
+{
+    public abstract class Enemy
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public bool IsFriendly { get; set; }
+    }
+}
